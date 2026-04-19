2c98e569c66cd483c4435761c3486cb3362d79b9
Imam Hossain <imamhosain95@gmail.com>
2026-04-20 00:42:04 +0600

    fix: turn app into a web server that shows hostname

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+const http = require("http");
 const os = require("os");
 
-const hostname = os.hostname();
-console.log(
-  `System Hostname: ${hostname}`,
+const server = http.createServer(
+  (req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(`
+        <h1>Load Balancer Test</h1>
+        <p>Served by container: <strong style="color:green">${os.hostname()}</strong></p>
+    `);
+  },
 );
-console.log("Github actions added");
+
+server.listen(3000, () => {
+  console.log(
+    `Server running on port 3000 | Hostname: ${os.hostname()}`,
+  );
+});
